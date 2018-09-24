ce9cfba0fb8c03a89f03ac326f1ac47aef3945c3
Dulip Withanage <dulip.withanage@gmail.com>
2018-09-24 14:16:08 +0200

    sesies browse

--- ompbrowse.py
+++ ompbrowse.py
@@ -5,8 +5,10 @@ Distributed under the GNU GPL v3. For full terms see the file
 LICENSE.md
 '''
 from gluon.html import *
-from gluon import current
+from gluon.http import HTTP
 from datetime import datetime
+from gluon import current
+from ompformat import seriesPositionCompare
 import collections
 
 
@@ -28,21 +30,24 @@ class Browser:
 
         self.t = len(self.submissions)
         self.total = self.t / p + 1 if self.t % p > 0 else self.t / p
-        self.navigation_select = self.get_navigation_select() if self.t> 20 else DIV()
-        self.navigation_list = self.get_navigation_list() if self.t> 20 else DIV()
+        self.navigation_select = self.get_navigation_select() if self.t > 20 else DIV()
+        self.navigation_list = self.get_navigation_list() if self.t > 20 else DIV()
         self.sort_select = self.get_sort_select()
         self.filter_select = self.get_filter_select()
 
     def get_submssion_sort_dict(self):
         # TODO do not change order  ,if changed heiup/views/catalog/index.html change del b.filter_select !!!!
         submission_sort = collections.OrderedDict()
-        submission_sort['newest_to_oldest'] = lambda s: min(
-            s.associated_items.get('publication_dates', [datetime(1, 1, 1)]))
-        submission_sort['oldest_to_newest'] = lambda s: min( s.associated_items.get('publication_dates', [datetime(1, 1, 1)]))
+        submission_sort['datePublished-1'] = [
+            lambda s: min(s.associated_items.get('publication_dates', [datetime(1, 1, 1)])), False]
+        submission_sort['datePublished-2'] = [
+            lambda s: min(s.associated_items.get('publication_dates', [datetime(1, 1, 1)])), True]
         # TODO may not work with editors
-        submission_sort['author'] = lambda s: s.associated_items.get('authors',[])[0].attributes.get('last_name','')
-        submission_sort['title'] = lambda s: s.settings.getLocalizedValue('title', self.locale).lower()
-        submission_sort['category'] = lambda s: s.associated_items.get('category').settings.getLocalizedValue('title', self.locale) if s.associated_items.get('category') else False
+        submission_sort['author'] = [lambda s: s.associated_items.get('authors', [])[0].attributes.get('last_name', ''),
+                                     False]
+        submission_sort['title-1'] = [lambda s: s.settings.getLocalizedValue('title', self.locale).lower(), False]
+        submission_sort['title-2'] = [lambda s: s.settings.getLocalizedValue('title', self.locale).lower(), True]
+
         return submission_sort
 
     def filter_submissions(self, s):
@@ -52,8 +57,6 @@ class Browser:
 
         s = filter(lambda s: category(s, self.filters.get('category')), s)
 
-        # submissions = filter(lambda s:   datetime.strptime(str(y),'%Y') <  min(s.associated_items.get('publication_dates',
-        # [datetime(1, 1, 1)])) <  datetime.strptime(str(y+1),'%Y') , submissions)
         return s
 
     def get_navigation_list(self):
@@ -75,7 +78,9 @@ class Browser:
         return DIV(button, ul, _class="btn-group pull-left")
 
     def get_sort_select(self, ul_class="btn-group pull-right"):
-        li = [LI(A(current.T(i).capitalize(), _href=URL('index?sort_by=' + str(i)))) for i in (self.submission_sort.keys())]
+
+        li = [LI(A(current.T(i).capitalize(), _href=URL('index?sort_by=' + str(i)))) for i in
+              (self.submission_sort.keys())]
         ul = UL(li, _class="dropdown-menu")
         button_cs = {"_type": "button", "_class": "btn btn-default dropdown-toggle", "_data-toggle": "dropdown",
                      "_aria-haspopup": "true", "_aria-expanded": "false"}
@@ -83,8 +88,19 @@ class Browser:
         return DIV(button, ul, _class=ul_class)
 
     def process_submissions(self, s):
-        reverse = True if 'newest_to_oldest' in self.sort_by else False
-        s = sorted(s, key=self.submission_sort.get(self.sort_by), reverse=reverse)
+
+        submission_sort = self.submission_sort.get(self.sort_by)
+        if self.sort_by == 'seriesPosition-1':
+            s = sorted(s, cmp=seriesPositionCompare, reverse=False)
+        elif self.sort_by == 'seriesPosition-2':
+            s = sorted(s, cmp=seriesPositionCompare, reverse=True)
+        elif self.sort_by == 'category':
+            s = sorted(s, key=lambda s: s.associated_items.get('category').settings.getLocalizedValue('title',
+                                                                                                      self.locale) if s.associated_items.get(
+                'category') else False, reverse=False)
+        else:
+            s = sorted(s, key=submission_sort[0], reverse=submission_sort[1])
+
         s = s[self.current * self.per_page:(self.current + 1) * self.per_page]
         return s
 
@@ -97,5 +113,5 @@ class Browser:
         button_cs = {"_type": "button", "_class": "btn btn-default dropdown-toggle", "_data-toggle": "dropdown",
                      "_aria-haspopup": "true", "_aria-expanded": "false"}
         button = TAG.button(current.T("Categories"), SPAN(_class='caret'), **button_cs)
-        u = UL(o,  _class="dropdown-menu")
-        return DIV(button, u,  _class=ul_class)
+        u = UL(o, _class="dropdown-menu")
+        return DIV(button, u, _class=ul_class)
